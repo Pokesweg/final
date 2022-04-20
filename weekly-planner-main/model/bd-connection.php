17e633da31cc2aa5f4483ff84090b4ee1f606cc2
Manwinder Paul <93801811+Pokesweg@users.noreply.github.com>
2022-04-20 11:56:05 -0400

    Add files via upload

---
+++ weekly-planner-main/model/bd-connection.php
@@ -0,0 +1,9 @@
+<?php 
+    // Creating variables to connect with the database
+    $servername = "sql1.njit.edu";
+    $username = "mp98";
+    $password = "Veenakshi1234.";
+    $dbname = "mp98";
+
+    $connection = new PDO($servername, $username, $password, $dbname);
+?>
